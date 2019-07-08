5ce57dad3c691fe5a0be40fda015e36b642bb090
Jhonatas Rodrigues <jhonatasrm@gmail.com>
2019-07-07 23:06:36 -0300

    Update JsonPlaceHolderAPI.java

--- src/exemploconsultaAPIusandoRetrofit/app/src/main/java/com/jhonatasrm/exemplo_consulta_api_usando_retrofit/Adapter/JsonPlaceHolderAPI.java
+++ src/exemploconsultaAPIusandoRetrofit/app/src/main/java/com/jhonatasrm/exemplo_consulta_api_usando_retrofit/Adapter/JsonPlaceHolderAPI.java
@@ -1,14 +1,15 @@
 package com.jhonatasrm.exemplo_consulta_api_usando_retrofit.Adapter;
 
 import com.google.gson.JsonObject;
-
-
+// import retrofit 2
 import retrofit2.Call;
 import retrofit2.http.GET;
 
-public interface JsonPlaceHolderAPI {
-    String apiKey = ""; // coloque a sua chave da API aqui
+//
 
+public interface JsonPlaceHolderAPI {
+    String apiKey = ""; // API Key
+    // address + API
     @GET("weather?q=london&appid="+apiKey)
     Call<JsonObject> getWeather();
 }
